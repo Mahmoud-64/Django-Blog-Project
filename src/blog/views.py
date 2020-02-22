9ed76d5728f10334b6c6d7d15f9d69b66bf10e89
mohamedAhmed97 <mohamed.a.ramadan23@gmail.com>
2020-02-22 16:22:17 +0200

    solve conflict of beta

--- src/blog/views.py
+++ src/blog/views.py
@@ -4,11 +4,9 @@ def index(request):
     return render(request,'landing_page/index.html',{})
 
 def blog(request):
-<<<<<<< HEAD
-    return render(request,'landing_page/body.html',{})
-=======
+
+    
     return render(request,'landing_page/base.html',{})
->>>>>>> d1f6ca8a5ec5a0d846d524447a79f58d98355f22
 
 def post(request):
     return render(request,'landing_page/post.html',{})
