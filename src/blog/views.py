fdb29edaf16065b7b7b021d6a607295bef93d5d8
Mahmoud-65 <mahmoudyoussef120@gmail.com>
2020-02-24 18:31:48 +0200

    adding some logic to move to specfic post using reserve function

--- src/blog/views.py
+++ src/blog/views.py
@@ -2,19 +2,13 @@ from django.shortcuts import render
 from post.models import Post
 
 def index(request):
-<<<<<<< HEAD
     queryset = Post.objects.all()
     return render(request,'landing_page/index.html',{'objects':queryset})
-=======
-    return render(request,'landing_page/index.html',{})
->>>>>>> d58c80eeb79e4f8b7b31d0dee256a7263f19e46a
 
 def blog(request):
-
-    
     return render(request,'landing_page/base.html',{})
 
-def post(request):
+def post(request,id):
     return render(request,'landing_page/post.html',{})
 
 
